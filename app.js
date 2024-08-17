6eabb846c0993a7bb2ed0a93ae960a1464febf01
devfawzey <devfawzey@gmail.com>
2024-08-17 10:23:58 +0300

    swagger_with_yaml

--- app.js
+++ app.js
@@ -16,14 +16,38 @@ const xss = require("xss-clean")
 const rateLimiter = require("express-rate-limit")
 
 // 
-const fs = require("fs")
 const swaggerUi = require("swagger-ui-express")
-const Yaml = require("yaml")
+const swaggerJsDoc = require("swagger-jsdoc")
+// const Yaml = require("yaml")
 // const yamlJS = require("yamljs")
 // const swaggerDocument = yamlJS.load("./swagger.yaml")
-// const swaggerDocument = require("./swagger.json")
-const file = fs.readFileSync("./swagger.yaml", "utf8")
-const swaggerDocument = Yaml.parse(file)
+const swaggerDocument = require("./swagger.json")
+const options = {
+ definition: {
+  openapi: "3.0.0",
+  info: {
+   title: "Library API",
+   version: "1.0.0",
+   description: "A simple Express Library API",
+   termsOfService: "http://example.com/terms/",
+   contact: {
+    name: "API Support",
+    url: "http://www.exmaple.com/support",
+    email: "support@example.com",
+   },
+  },
+  servers: [
+   {
+    url: "https://jobs-api-topaz.vercel.app/",
+    description: "My API Documentation",
+   },
+  ],
+ },
+ // This is to call all the file
+ apis: ["./swagger.yaml"],
+};
+
+const specs = swaggerJsDoc(options);
 const CSS_URL =
  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 
@@ -44,7 +68,7 @@ const { errorHandlerMiddleware, notFoundMiddleware } = require("./middleware")
 
 // routes
 app.get("/", (req, res) => res.send("<h1>Jobs Api</h1> <a href='/api-docs'>API-Documents</a>"))
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs, { customCssUrl: CSS_URL }))
 app.use("/api/v1/jobs/", authenticatedUser, jobRoute)
 app.use("/api/v1/auth", authRoute)
 app.use(notFoundMiddleware)
